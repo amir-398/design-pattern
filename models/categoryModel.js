817c05a2fb95adfee0dfedbf79fe2d7bc34d72b6
Amir <amir.398@hotmail.fr>
2024-01-16 09:37:41 +0100

    functionality

---
+++ models/categoryModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let categorySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Category", categorySchema);
