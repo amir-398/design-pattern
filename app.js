cc0f13b4ccd39965a95287b9c5ec5a8467747943
Gana <ganafall9498@gmail.com>
2024-01-17 12:02:24 +0100

    Add category route to app.js

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const port = 3000; // Définition du port sur lequel le serveur écoutera
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocs = require("./swaggerConfig/swaggerConfig");
 const movieRoute = require("./routes/movieRoute");
+const categoryRoute = require("./routes/categoryRoute");
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/design_patern");
 app.use(express.urlencoded());
 app.use(express.json());
 app.use("/", movieRoute);
+app.use("/", categoryRoute);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 // Démarrage du serveur Express et écoute sur le port spécifié
 app.listen(port, () => {
