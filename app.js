4685d1d120036f69057624c42f44dbbbd1116a6c
Amir <amir.398@hotmail.fr>
2024-01-17 10:23:39 +0100

    Remove xmlparser middleware and related dependencies

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const xmlparser = require("express-xml-bodyparser");
 const app = express();
 const port = 3000; // Définition du port sur lequel le serveur écoutera
 const swaggerUi = require("swagger-ui-express");
@@ -9,13 +8,9 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/design_patern");
 app.use(express.urlencoded());
 app.use(express.json());
-//app.use(xmlparser());
-// Définition de la première route pour la racine de l'application
-
 app.use("/", movieRoute);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 // Démarrage du serveur Express et écoute sur le port spécifié
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`); // Affiche un message dans la console lorsque le serveur démarre
 });
-
